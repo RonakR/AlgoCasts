fd712c0ba81fe93a6b4a705b078df9f5f0f6399b
Ronak Raithatha <ronaksraithatha@gmail.com>
2019-01-13 15:04:01 -0500

    solved reducestring

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,41 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+    
+    // My shitty soln.
+
+    // if (str == '') return str;
+    // let arr = [];
+    // for (let i=0; i<str.length; i++) {
+    //     arr.unshift(str.charAt(i));
+    // }
+    // return arr.join('');
+
+    // ---------------------------------------
+
+    // First Soln:
+    // Array.prototype.reverse()
+
+    // return str.split('').reverse().join('');
+
+    // ---------------------------------------
+
+    // Second Soln:
+    // Create an empty string 'reversed', add letter + existing string.
+
+    // let reversed = '';
+    // for ( let char of str) {
+    //     reversed = char + reversed;
+    // }
+    // return reversed;
+
+    // ---------------------------------------
+
+    // Third Soln (complicated):
+    // Reduce Array 
+
+    return str.split('').reduce((rev, char) => char + rev, '');
+}
 
 module.exports = reverse;
