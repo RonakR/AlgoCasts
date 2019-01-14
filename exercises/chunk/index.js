154a525a65d49408d3416ac6fa86557dde579cdb
Ronak Raithatha <ronaksraithatha@gmail.com>
2019-01-13 21:07:32 -0500

    soln to chunk

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,45 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+// My Soln
+// Uses splice
+// function chunk(array, size) {
+//     const numChunks = Math.trunc(array.length / size);
+//     const retArr = [];
+
+//     for (let i = 0; i < numChunks; i++) {
+//         retArr.push(array.splice(0, size));
+//     }
+//     if (array % size !== 0) retArr.push(array)
+//     return retArr;
+// }
+
+// First Soln
+// Chuncks in chucked to see if they have enough chunk.
+// function chunk(array, size) {
+//     const chuncked = [];
+//     for (let element of array) {
+//         const last = chuncked[chuncked.length - 1];
+
+//         if (!last || last.length === size) {
+//             chuncked.push([element]);
+//         } else {
+//             last.push(element);
+//         }
+//     }
+//     return chuncked;
+// }
+
+// Second Soln
+// Uses slice
+function chunk(array, size) {
+    const chunked = [];
+    let index = 0;
+    while (index < array.length) {
+        chunked.push(array.slice(index, index + size));
+        index += size;
+    }
+    return chunked;
+}
 
 module.exports = chunk;
