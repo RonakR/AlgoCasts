2b2bae5e71a0f28b2b336ccd98678a00cd240f15
Ronak Raithatha <ronaksraithatha@gmail.com>
2019-01-16 14:04:57 -0500

    solved level widths'

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,21 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+    const arr = [root, 's'];
+    const width = [0];
+    while (arr.length > 1) {
+        const node = arr.shift();
+        if (node === 's') {
+            arr.push('s');
+            width.push(0);
+        } else {
+            width[width.length - 1]++;
+            arr.push(...node.children);
+        }
+    }
+
+    return width;
+}
 
 module.exports = levelWidth;
