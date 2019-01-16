2db16442d4b4f1f5096e5ab317815b4f07dca7cb
Ronak Raithatha <ronaksraithatha@gmail.com>
2019-01-16 10:53:36 -0500

    solved pyramid

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,51 @@
 //       ' ### '
 //       '#####'
 
+// My Soln
+// function pyramid(n) {
+//     for (let i = 1; i <= n; i++) {
+//         console.log(' '.repeat(n - i) + '#'.repeat(i*2 - 1) + ' '.repeat(n - i));
+//     }
+// }
+
+
+function pyramid(n, row = 0, level = '') {
+    if (n === row) return;
+    if (level.length === (n*2 - 1)) {
+        console.log(level);
+        return pyramid(n, row + 1);
+    }
+
+    if (level.length < n*2 - 1) {
+
+    } else {
+        return pyramid(n, row + 1)
+    }
+}
+
+// First Soln 
+// Iterative 
+function pyramid(n) {
+    const midPoint = Math.floor((2*n - 1) / 2);
+
+    for (let row = 0; row < n; row++) {
+        let level = '';
+
+        for (let col = 0; col < n*2 - 1; col++) {
+            if (midPoint - row <= col && midPoint + row >= col) {
+                level += '#';
+            } else {
+                level += ' ';
+            }
+        }
+        console.log(level);
+    }
+
+}
+
+// Second Soln
+// Recursive 
 function pyramid(n) {}
 
 module.exports = pyramid;
+ 
